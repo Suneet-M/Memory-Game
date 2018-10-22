cf01ca33055c2bdb4646a3ec531f776a7f8ab45b
Suneet Mehta <43340251+Suneet-M@users.noreply.github.com>
2018-10-22 18:10:22 +0530

    Add Click to show event on cards

--- js/app.js
+++ js/app.js
@@ -36,3 +36,15 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ let CardList = document.querySelector('.deck');
+
+CardList.addEventListener('click' , 
+	function (e) {
+		e.target.classList.add('open','show');
+
+
+
+
+
+});
